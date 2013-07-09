5f080b8107b25f2f801969d56733f36871a5a313
Craig Kollegger <craig.kollegger@gmail.com>
2013-07-09 10:55:57 +0000

    Changed web.js to use buffers

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var buffer = new Buffer(fs.readFile(index.html));
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.stString);
 });
 
 var port = process.env.PORT || 5000;
