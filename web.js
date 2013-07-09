fcf787598a57783ef199675d735f4a83d1a8d80b
Craig Kollegger <craig.kollegger@gmail.com>
2013-07-09 12:40:55 +0000

    Latest changes

--- web.js
+++ web.js
@@ -1,10 +1,13 @@
 var express = require('express');
 
-var app = express.createServer(express.logger());
-var buffer = new Buffer(fs.readFile("index.html"));
+var buffer = new Buffer(fs.readFileSync('index.html', 'utf-8'));
 
 app.get('/', function(request, response) {
-  response.send(buffer.stString);
+  var file = require('fs');
+  var buffer = new Buffer(256);
+  buffer = fs.readFileSync('index.html', 'utf-8'));
+
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
