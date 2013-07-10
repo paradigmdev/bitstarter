96bd0305563ef5627b89452b192849a69a491609
Craig Kollegger <craig.kollegger@gmail.com>
2013-07-10 09:23:10 +0000

    some additional playing around.

--- grader.js
+++ grader.js
@@ -24,8 +24,12 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+var sys = require('util');
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "www.google.com";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -36,6 +40,25 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
+var assertUrlExists = function(inurl) {
+    var sys = require('util'),                                                                                                                                                   
+    var getURL = function(url) { 
+    var sys = require('util'), 
+    var rest = require('restler'); 
+    
+    rest.get(inurl).on('complete', function(result) {                                                                                                              
+	if (result instanceof Error) {                                                                                                                 
+	    sys.puts('Error: ' + result.message);     
+	    this.retry(5000); // try again after 5 sec
+	} else {                                                                  
+	    return result;                                                       
+        }                                                                       
+    });                                                                                                                                                                         
+};
+
+	
+};
+ 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
@@ -55,6 +78,12 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var loadURL = function(url) {
+    return rest.get(url).on('complete', function(result, response){
+	
+    });    
+};
+    
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -65,6 +94,7 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL to check', clone(assertUrlExists), URL_DEFAULT)
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
