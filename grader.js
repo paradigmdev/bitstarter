5606b8392489fe80faddd16d63ee4b2c6cd93ed0
Craig Kollegger <craig.kollegger@gmail.com>
2013-07-11 09:56:15 +0000

    publishing my grader.js to ask for help

--- grader.js
+++ grader.js
@@ -27,38 +27,25 @@ var cheerio = require('cheerio');
 var rest = require('restler');
 var sys = require('util');
 
-var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
-var URL_DEFAULT = "www.google.com";
+var FILE_DEFAULT = "index.html";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
-        console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+	console.log("%s does not exist. Exiting.", instr);
+	process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
 };
 
-var assertUrlExists = function(inurl) {
-    var sys = require('util'),                                                                                                                                                   
-    var getURL = function(url) { 
-    var sys = require('util'), 
-    var rest = require('restler'); 
-    
-    rest.get(inurl).on('complete', function(result) {                                                                                                              
-	if (result instanceof Error) {                                                                                                                 
-	    sys.puts('Error: ' + result.message);     
-	    this.retry(5000); // try again after 5 sec
-	} else {                                                                  
-	    return result;                                                       
-        }                                                                       
-    });                                                                                                                                                                         
+// load the URL though Cheerio
+var cheerioURL = function(url) {
+    rest.get(url).on('complete', function(result) {
+	return cheerio.load(result);
+    });
 };
 
-	
-};
- 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
@@ -67,23 +54,22 @@ var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtml = function(htmlfile, urlpath, checksfile) {
+    if (program.url) {
+      $ = cheerioURL(urlpath);
+    } else {
+      $ = cheerioHtmlFile(htmlfile);
+    }
+
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
-        out[checks[ii]] = present;
+	var present = $(checks[ii]).length > 0;
+	out[checks[ii]] = present;
     }
     return out;
 };
 
-var loadURL = function(url) {
-    return rest.get(url).on('complete', function(result, response){
-	
-    });    
-};
-    
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -92,13 +78,13 @@ var clone = function(fn) {
 
 if(require.main == module) {
     program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .option('-u, --url <url>', 'URL to check', clone(assertUrlExists), URL_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), FILE_DEFAULT)
+	.option('-u, --url <url>', 'URL to check')
+	.parse(process.argv);
+    var checkJson = checkHtml(program.file, program.url, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
-    exports.checkHtmlFile = checkHtmlFile;
+    exports.checkHtml = checkHtml;
 }
